c32eb7279ae0b63980bf09328765635964b9576f
makinde timilehin <makindetimi@gmail.com>
2021-06-18 15:39:10 +0100

    creating product model

---
+++ models/product.model.js
@@ -0,0 +1,34 @@
+const { Sequelize, Op, Model, DataTypes } = require("sequelize")
+const { v4 } = require("uuid");
+
+module.exports = (sequelize, Sequelize) => {
+    const Product = sequelize.define("product", {
+      id:{
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.TEXT,
+ 	      defaultValue: Sequelize.UUIDV4
+      },
+      images:{
+        type:DataTypes.JSONB, // NOT REALLY EFFECIENT BUT WILL WORK WELL FOR SMALL PROJECTS
+        defaultValue:[],
+      },
+      name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      desc: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      location:{
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      category: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    });
+    return Product;
+  };
